917dc7c1525ff67526ac96843df88ea4db2397ff
MChance176 <matt.chance176@gmail.com>
2024-11-06 13:32:33 -0500

    Added App.tsx, App.css

---
+++ client/src/App.tsx
@@ -0,0 +1,17 @@
+import { FC } from 'react'
+import './App.css'
+
+const App: FC = () => {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <h1>Online Checkers</h1>
+      </header>
+      <main className="app-main">
+        {/* Game board will go here once we create it */}
+      </main>
+    </div>
+  )
+}
+
+export default App
